210824256c687d65f0a673525efde808a28fe9f3
Vasileios Pasialiokis <whiterocket@outlook.com>
2023-09-24 22:16:02 +0300

    clean up after deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 scp -P 2022 *deb root@sexualise.it:/root
 ssh -p 2022 root@sexualise.it 'apt install --no-install-recommends --no-install-suggests -f ./*deb && rm -v *deb'
+\rm -v -- ./*deb
